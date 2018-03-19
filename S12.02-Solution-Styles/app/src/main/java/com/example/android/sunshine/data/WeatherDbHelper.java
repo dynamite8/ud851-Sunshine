4f465db2af87f69c6af974c9f6e35ea41dd9d862
Jenny Chang Ho <orthographic@gmail.com>
2018-03-18 22:34:52 -0400

    Completed S12.02-Exercise-Styles

--- S12.02-Solution-Styles/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S12.02-Solution-Styles/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -43,7 +43,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
      * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
      * version your databases.
      */
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
